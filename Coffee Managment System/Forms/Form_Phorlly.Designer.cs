5bde2f7f096bcc10f3334f88b596989b6419e5b1
Phorlly <lannphorlly.698896@gmail.com>
2022-03-16 23:40:00 +0700

    Push by branch phally

--- Coffee Managment System/Forms/Form_Phorlly.Designer.cs
+++ Coffee Managment System/Forms/Form_Phorlly.Designer.cs
@@ -29,10 +29,19 @@ namespace Coffee_Managment_System.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form_Phorlly
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(789, 585);
+            this.Font = new System.Drawing.Font("Khmer OS", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form_Phorlly";
             this.Text = "Form_Phorlly";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
